d85ec39d7bda96390ccec7ea2ce97472dc0f4ee3
Surjendu Debnath <surjendudebnath907@gmail.com>
2021-08-12 13:22:54 +0530

    initilazing

---
+++ STONE PAPER SCISSOR GAME.py
@@ -0,0 +1,53 @@
+import random
+
+
+# Snake Water Gun or Rock Paper Scissors
+def gameWin(comp, you):
+    # If two values are equal, declare a tie!
+    if comp == you:
+        return None
+
+    # Check for all possibilities when computer chose s
+    elif comp == 's':
+        if you=='c':
+            return False
+        elif you=='p':
+            return True
+    
+    # Check for all possibilities when computer chose w
+    elif comp == 'c':
+        if you=='p':
+            return False
+        elif you=='s':
+            return True
+    
+    # Check for all possibilities when computer chose g
+    elif comp == 'p':
+        if you=='s':
+            return False
+        elif you=='c':
+            return True
+
+
+print("Comp Turn: Stone(s) Paper(p) or Scissor(c)?")
+randNo = random.randint(1, 3) 
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'c'
+elif randNo == 3:
+    comp = 'p'
+
+
+you = input("Your Turn: Stone(s) Paper(p) or Scissor(c)?")
+a = gameWin(comp, you)
+
+print(f"Computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
